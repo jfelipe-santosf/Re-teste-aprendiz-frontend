4a82b42370e98e544e3e22347677af75eb2fafae
jfelipe-santosf <josefelipesantosfreitas@gmail.com>
2024-07-04 22:38:57 -0300

    Versão Mob OK

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    dots: false,
+    stagePadding: 75,
+    margin: 0,
+    responsive:{
+        1:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
